4b2346893d30fd501a4ad05a5107e9cf1c395a8b
Taylor <taylor@stykocompany.com>
2022-01-25 14:07:40 -0600

    Add README and improve en-US translations (#4)
    Co-authored-by: Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>

---
+++ README.md
@@ -0,0 +1,36 @@
+# Remote for Spotify (Playback control)
+
+“Playback control” is a perfect addition to the official Spotify integration. Do you often play music from Spotify on your PC/TV/other device and find it annoying that Alexa can't control playback there? Well, with this skill, she can!
+
+> **Please note:**
+> For most of the features this skill offers, a Spotify Premium account is required. If you've just purchased Premium, you might need to disable and re-enable the skill in order to use Premium features.
+Google Cast devices might not be available for control.
+
+## Enabling the skill
+You can either
+- use this globalized Amazon link: [https://bit.ly/3aJg8jq](https://bit.ly/3aJg8jq) (provided by [bestazon.io](https://bestazon.io)) or
+- search for "Remote for Spotify" in the Alexa app.
+
+## Features
+- Play on device (e.g. “play on Living Room”, if a device of yours is called “Living Room”)
+- Seek a timestamp in a song (e.g. “seek to one minute and ten seconds”)
+- Set volume (in percent)
+- Play/Pause
+- Next/Previous song
+- Shuffle on/off
+- “What song is this?”
+
+
+## Player Controls
+You can use Alexa’s standard voice commands to control the player during playback. Just say `Alexa, tell Playback control to Pause`. You will need to say `Tell Playback control` or `Ask Playback control` when using these commands.
+* `Play`
+* `Pause`
+* `Next`
+* `Previous`
+* `Shuffle on/off`
+	* `"turn Shuffle on/off"`
+
+## Support and feedback:
+Send an email to [alexa@nicolaiweitkemper.de](mailto:alexa@nicolaiweitkemper.de) or [open an issue on GitHub](https://github.com/NicoWeio/Alexa-Spotify-Remote/issues/new).
+
+> **Disclaimer**: This skill is not created by, affiliated with, or supported by Spotify.
--- language_en-US.js
+++ language_en-US.js
@@ -1,29 +1,29 @@
 module.exports = {
   translation: {
     WELCOME: "Welcome to “Playback control”, the unofficial remote for Spotify. What would you like to do?",
-    WELCOME_NO_TOKEN: "Welcome to “Playback control”, the unofficial remote for Spotify. In order to use this skill, you need to link it to Spotify. Just open the Alexa app and follow the instructions.",
+    WELCOME_NO_TOKEN: "Welcome to “Playback control”, the unofficial remote for Spotify. In order to use this skill, you need to link your Spotify account. To do this just open the Alexa app and follow the instructions.",
     WELCOME_REPROMPT: "What would you like to do? If you need help, just say “help”.",
-    HELP: "Here is a short introduction in den Skill “Playback control”. It enables you to control playback on Spotify, even if it's not currently playing on the Alexa device you talk to. The following features are available: Playback on a certain device, pause, changing volume, skipping to the next or previous song, seeking in the currently playing track, and toggling shuffle. You can also ask me what is currently playing. How can I help you now?",
+    HELP: "Here is a short introduction of “Playback control”. It enables you to control playback on Spotify, even if it's not currently playing on an Alexa device. The following features are available: Playback on a specific device, pause, changing volume, skipping to the next or previous song, seeking in the currently playing track, and toggling shuffle. You can also ask me what song is currently playing. How can I help you now?",
     HELP_REPROMPT: "If you need further assistance, take a look at the skill's description in the Alexa app. How can I help you now?",
     GOODBYE: "Goodbye!",
     GOODBYE_THANKS: "Goodbye and thanks for using “Playback control”.",
     REFLECTOR: "You just triggered {{intentName}}.",
     FALLBACK: "I don't know how to help you with that. Try rephrasing your question.",
-    ERROR: "Sorry, that did not work. Please try again.",
-    ERROR_REPROMPT: "Please try again. For further assistance, take a look at the skill's description in the Alexa app or write an e-mail to “alexa@nicolaiweitkemper.de”.",
+    ERROR: "Sorry, that didn't work. Please try again.",
+    ERROR_REPROMPT: "Please try again. For further assistance, take a look at the skill's description in the Alexa app.",
 
     GET_CURRENTLY_PLAYING: "This is “{{itemName}}” by “{{artists}}”, playing on “{{deviceName}}”.",
     NOTHING_PLAYING: "There is no playback at the moment.",
 
-    PLAY_SUCCESS: "Okay, I start the playback.",
-    PLAY_ERROR: "That did not work. Maybe playback is already running?",
+    PLAY_SUCCESS: "Okay, playing.",
+    PLAY_ERROR: "That didn't work. Maybe playback is already running?",
 
     PLAYONDEVICE_SUCCESS: "Okay, playing on {{deviceName}}",
     PLAYONDEVICE_NO_MATCH: "I was unable to find a matching device. Please try it again.",
     PLAYONDEVICE_NO_MATCH_REPROMPT: "Please try it again. Give me the name of the device you would like to play music on.",
 
     PAUSE_SUCCESS: "Okay, pausing.",
-    PAUSE_ERROR: "That did not work. Maybe playback is already paused?",
+    PAUSE_ERROR: "That didn't work. Is playback already paused?",
 
     NEXTSONG_SUCCESS: "Okay, next song.",
 
@@ -36,7 +36,7 @@ module.exports = {
 
     SEEK_SUCCESS: "Okay.",
 
-    ERR_NO_TOKEN: "You haven't linked this skill to Spotify yet. In order to do that, open the Alexa app and follow the instructions.",
+    ERR_NO_TOKEN: "You haven't linked this skill to your Spotify account yet. In order to do that, open the Alexa app and follow the instructions.",
     ERR_UNKNOWN: "An unknown error occured.",
 
     // https://developer.spotify.com/documentation/web-api/reference/object-model/#player-error-object
@@ -54,10 +54,10 @@ module.exports = {
       // ALREADY_PLAYING: "The track should not be restarted if the same track and context is already playing, and there is a resume point.",
       // RATE_LIMITED: "You are currently rate limited due to too frequent track play, also known as cat-on-the-keyboard spamming.",
       REMOTE_CONTROL_DISALLOW: "The context cannot be remote-controlled.",
-      DEVICE_NOT_CONTROLLABLE: "It is not possible to remote control the device.",
-      VOLUME_CONTROL_DISALLOW: "It is not possible to remote control the device’s volume.",
-      NO_ACTIVE_DEVICE: "I don't see any active device to perform this action on.",
-      PREMIUM_REQUIRED: "Unfortunately, this request is prohibited for non-premium users.",
+      DEVICE_NOT_CONTROLLABLE: "It's not possible to remote control the device.",
+      VOLUME_CONTROL_DISALLOW: "It's not possible to remote control the device’s volume.",
+      NO_ACTIVE_DEVICE: "I don't see an active device to perform this action on.",
+      PREMIUM_REQUIRED: "Unfortunately, you need a Spotify Premium account to do this.",
       UNKNOWN: "Hm, that didn't work out. Maybe you've tried something like starting playback while it's already playing.",
     }
   },
